0bf58d9a42d53880fa3358df8f14c7876ffe57ff
Mark Kim <marksk82@gmail.com>
2020-08-10 15:47:59 +0900

    Update Train.py

--- TF2.0/Cutmix/Train.py
+++ TF2.0/Cutmix/Train.py
@@ -5,85 +5,80 @@ import tensorflow as tf
 from tensorflow.keras import datasets, layers, models, utils, Model
 from tensorflow.keras.layers import Dense, Flatten, Conv2D
 from ResNet import ResNet
-from Functions import normalize, train_augment, test_augment, rand_bbox, model_save
+from Functions import normalize, train_augment, test_augment, rand_bbox, learning_rate_schedule
 import numpy as np
 import argparse
 import os
 import time
-
+import datetime
 
 parser = argparse.ArgumentParser(description='Tensorflow implementation of Cutmix on CIFAR-10 / CIFAR-100 datasets')
 
-parser.add_argument('--epochs', default=90, type=int, metavar='N',
+parser.add_argument('--epochs', default=3, type=int, metavar='N',
                     help='number of total epochs to run')
 parser.add_argument('-b', '--batch_size', default=128, type=int,
-                    metavar='N', help='mini-batch size (default: 256)')
+                    metavar='N', help='mini-batch size (default: 128)')
 parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
                     help='momentum')
-parser.add_argument('--print-freq', '-p', default=1, type=int,
+parser.add_argument('--print_freq', '-p', default=1, type=int,
                     metavar='N', help='print frequency (default: 10)')
 parser.add_argument('--depth', default=32, type=int,
                     help='depth of the network (default: 32)')
 parser.add_argument('--no-bottleneck', dest='bottleneck', action='store_false',
                     help='to use basicblock for CIFAR datasets (default: bottleneck)')
 parser.add_argument('--dataset', dest='dataset', default='cifar100', type=str,
-                    help='dataset (options: cifar10, cifar100, and imagenet)')
+                    help='dataset (options: cifar10 or cifar100)')
 parser.add_argument('--beta', default=0, type=float,
                     help='hyperparameter beta')
 parser.add_argument('--cutmix_prob', default=0, type=float,
                     help='cutmix probability')
-parser.add_argument('--boundaries', default=[0, 100, 150, 200], type=list,
+parser.add_argument('--boundaries', default=[1, 2], type=list, # [0, 100, 150, 200]
                     help='epochs for learning rate decay boundaries')
-parser.add_argument('--lr_values', default=[0.2, 0.1, 0.05, 0.01], type=list,
+parser.add_argument('--lr_values', default=[0.2, 0.1, 0.1], type=list, # [0.2, 0.1, 0.05, 0.01]
                     help='learning rates for corresponding boundaries')
+parser.add_argument('--ckpt_dir', dest='ckpt_dir', default='./ckpt_dir/', type=str,
+                    help='checkpoint saving folder')
+parser.add_argument('--tensorboard_dir', dest='tensorboard_dir', default='./tensorboard_dir/', type=str,
+                    help='tensorboard summary folder')
 parser.add_argument('--verbose', default=1, type=int,
                     help='print training process to screen')
 
 parser.set_defaults(bottleneck=True)
-best_err1 = 100
-best_err5 = 100
 AUTO = tf.data.experimental.AUTOTUNE
 template = 'Epoch: {:03d} / {:03d}:, LR: {:.10f}, Train loss: {:.3f}, Train top1 err: {:.3f}, Train top5 err: {:.3f}, Test loss: {:.3f}, Test top1 err: {:.3f}, Test top5 err: {:.3f}, Time: {:.3f}'
 
 
-
-def learning_rate_schedule(boundaries, values, train_labels):
-    lr_bound = []
-    for bound in boundaries:
-      lr_bound.append(bound * len(train_labels))      
-    return tf.keras.optimizers.schedules.PiecewiseConstantDecay(lr_bound, values)
-  
-  
-@tf.function
-def train_cutmix_image(image_cutmix, target_a, target_b, lam):
-  with tf.GradientTape() as tape:
-    output = model(image_cutmix, training=True) 
-    loss = criterion(target_a, output) * lam + criterion(target_b, output) * (1. - lam)
-  gradients = tape.gradient(loss, model.trainable_variables)
-  optimizer.apply_gradients(zip(gradients, model.trainable_variables)) 
-  return loss, output
-
-
-@tf.function
-def train_original_image(image, target):
-  with tf.GradientTape() as tape:
-    output = model(image, training=True)
-    loss = criterion(target, output)
-  gradients = tape.gradient(loss, model.trainable_variables)
-  optimizer.apply_gradients(zip(gradients, model.trainable_variables)) 
-
-  return loss, output
-
-
 # main
 def main():
+        
+  @tf.function
+  def train_cutmix_image(image_cutmix, target_a, target_b, lam):
+    with tf.GradientTape() as tape:
+      output = model(image_cutmix, training=True) 
+      loss = criterion(target_a, output) * lam + criterion(target_b, output) * (1. - lam)
+    gradients = tape.gradient(loss, model.trainable_variables)
+    optimizer.apply_gradients(zip(gradients, model.trainable_variables)) 
+    return loss, output
 
-  global args, best_err1, best_err5
+
+  @tf.function
+  def train_original_image(image, target):
+    with tf.GradientTape() as tape:
+      output = model(image, training=True)
+      loss = criterion(target, output)
+    gradients = tape.gradient(loss, model.trainable_variables)
+    optimizer.apply_gradients(zip(gradients, model.trainable_variables)) 
+    return loss, output
+
+
+#   global args, best_err1, best_err5
   args = parser.parse_args()
+  assert len(args.boundaries)+1 == len(args.lr_values), "Epochs and Learning rates are not matched"
 
   if args.dataset == 'cifar10':
     (train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
     num_classes = 10
+    
   elif args.dataset == 'cifar100':
     (train_images, train_labels), (test_images, test_labels) = datasets.cifar100.load_data()
     num_classes = 100
@@ -104,9 +99,15 @@ def main():
   
   # loss & optimizer
   criterion = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
-  lr_schedule = learning_rate_schedule(args.boundaries, args.lr_values, train_labels)    
+  lr_schedule = learning_rate_schedule(args.boundaries, args.lr_values, train_labels, args.batch_size, args.epochs)    
   optimizer = tf.keras.optimizers.SGD(lr_schedule, momentum=args.momentum, nesterov=True)
- 
+    
+  # ckpt manager
+  current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+  ckpt_dir = os.path.join(args.ckpt_dir, current_time)
+  ckpt = tf.train.Checkpoint(model=model, optimizer=optimizer)
+  ckpt_manager = tf.train.CheckpointManager(ckpt, ckpt_dir, max_to_keep=1)
+
   # metrics 
   train_loss = tf.keras.metrics.Mean(name='train_loss')
   train_accuracy_top1 = tf.keras.metrics.SparseTopKCategoricalAccuracy(k=1, name='train_accuracy_top1')
@@ -117,13 +118,15 @@ def main():
   test_accuracy_top5 = tf.keras.metrics.SparseTopKCategoricalAccuracy(k=5, name='test_accuracy_top5')
 
   # tensorboard writer 
-  current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-  train_log_dir = 'logs/gradient_tape/' + current_time + '/train'
-  test_log_dir = 'logs/gradient_tape/' + current_time + '/test'
+  train_log_dir = os.path.join(args.tensorboard_dir, current_time, 'train')
+  test_log_dir = os.path.join(args.tensorboard_dir, current_time, 'test')
   train_summary_writer = tf.summary.create_file_writer(train_log_dir)
   test_summary_writer = tf.summary.create_file_writer(test_log_dir)
+  print(f'******* Train result log written to {train_log_dir} ******')
+  print(f'******* Test result log written to {test_log_dir} ******')
+ 
 
-  template = 'Epoch: {:03d} / {:03d}:, LR: {:.3f}, Train loss: {:.3f}, Train top1 err: {:.3f}, Train top5 err: {:.3f}, Test loss: {:.3f}, Test top1 err: {:.3f}, Test top5 err: {:.3f}, Time: {:.3f}'
+  template = '******* Epoch: {:03d} / {:03d}:, LR: {:.3f}, Train loss: {:.3f}, Train top1 err: {:.3f}, Train top5 err: {:.3f}, Test loss: {:.3f}, Test top1 err: {:.3f}, Test top5 err: {:.3f}, Time: {:.3f}'
     
   # custom training
   for epoch in range(args.epochs):
@@ -173,27 +176,29 @@ def main():
       test_loss(loss)
       test_accuracy_top1(target, output)
       test_accuracy_top5(target, output)
-      
-    # writing to tensorboard
-    with train_summary_writer.as_default():
-      tf.summary.scalar('train_loss', train_loss.result(), step=epoch)
-      tf.summary.scalar('train_err_top1', (1-train_accuracy_top1.result())*100, step=epoch)
-      tf.summary.scalar('train_err_top5', (1-train_accuracy_top5.result())*100, step=epoch)
+      print((1-test_accuracy_top5.result())*100)
 
-    with test_summary_writer.as_default():
-      tf.summary.scalar('test_loss', test_loss.result(), step=epoch)
-      tf.summary.scalar('test_err_top1', (1-test_accuracy_top1.result())*100, step=epoch)
-      tf.summary.scalar('test_err_top5', (1-test_accuracy_top5.result())*100, step=epoch)
 
     train_err1 = (1-train_accuracy_top1.result())*100
     train_err5 = (1-train_accuracy_top5.result())*100
     test_err1 = (1-test_accuracy_top1.result())*100
     test_err5 = (1-test_accuracy_top5.result())*100
+      
+    # writing to tensorboard
+    with train_summary_writer.as_default():
+      tf.summary.scalar('train_loss', train_loss.result(), step=epoch)
+      tf.summary.scalar('train_err_top1', train_err1, step=epoch)
+      tf.summary.scalar('train_err_top5', train_err5, step=epoch)
+
+    with test_summary_writer.as_default():
+      tf.summary.scalar('test_loss', test_loss.result(), step=epoch)
+      tf.summary.scalar('test_err_top1', test_err1, step=epoch)
+      tf.summary.scalar('test_err_top5', test_err5, step=epoch)
 
     # print metrics to screen
-    if ((epoch+1) % PRINT_FREQ_EPOCH == 0) & (VERBOSE==1):
+    if ((epoch+1) % args.print_freq == 0) & (args.verbose==1):
       print(template.format(epoch+1, 
-                            NUM_EPOCHS,
+                            args.epochs,
                             optimizer.learning_rate(optimizer.iterations).numpy(),
                             train_loss.result(),
                             train_err1,
@@ -203,13 +208,22 @@ def main():
                             test_err5,
                             time.time()-s_time))
  
-    # saving only the best model 
-    is_best = test_err1 <= best_err1
-    best_err1 = min(test_err1, best_err1)
-    if is_best:
-        print('Model being saved...')  
-        model.save(MODEL_DIR, save_format='tf')  
-        print('Best accuracy (top-1 and 5 error):', best_err1, best_err5)
+#     # saving only the best model 
+#     is_best = test_err1 <= best_err1
+#     best_err1 = min(test_err1, best_err1)
+#     if is_best:
+#       print('Model being saved...')  
+#       manager.save(checkpoint_number=optimizer.iterations)
+#       print('Best accuracy (top-1 and 5 error):', best_err1, best_err5)
+
+  # save only the last epoch model
+  ckpt_manager.save(checkpoint_number=optimizer.iterations)
+  print(f'******* Model saved to {ckpt_dir} ******') 
+  print(f'******* Test top-1 err: {test_err1}  top-5 err: {test_err5}')
+
 
 if __name__ == '__main__':
     main()
+
+    
+    
